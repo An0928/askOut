11ba31aeaf480ce49feff4171070898f79165157
An0928 <layannann@gmail.com>
2026-09-16 16:50:24 +0800

    Update picture

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
+/*
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/*const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,3 +10,17 @@ const nextConfig = {
 }
 
 export default nextConfig
+*/
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // 若儲存庫名稱為 my-invitation，這裡填入 '/my-invitation'
+  // 若儲存庫是帳號首頁（如 username.github.io），則維持空字串 ''
+  basePath: process.env.NODE_ENV === 'production' ? 'askOut' : '',
+};
+
+export default nextConfig;
